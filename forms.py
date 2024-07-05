e83293e8faeb2481d1c20032e6496083fa9078b4
Joseph <jmkl0987@gmail.com>
2024-07-05 16:46:04 +0530

    Change in forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, URL, Email, Length
 from flask_ckeditor import CKEditorField
 
@@ -15,7 +15,7 @@ class CreatePostForm(FlaskForm):
 
 # TODO: Create a RegisterForm to register new users
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(message="Enter a valid Email")])
+    email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message="The password should have 6 characters")])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField()
