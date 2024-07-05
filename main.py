c67d2e84ef81f4f0fc2fceed6c154a9f00bb9f65
Joseph <jmkl0987@gmail.com>
2024-07-05 15:50:57 +0530

    Change in main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ my_email = 'youremail'
 password = "yourpassword"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'yoursecretkey'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -269,4 +269,4 @@ def unauthorized():
     return render_template('admin.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
